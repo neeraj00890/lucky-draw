9869141ee1a078962833c09d2f2c5cdeeb85f7b0
neeraj00890 <kumar74312@gmail.com>
2022-04-08 18:00:38 +0530

    fix: cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const dotEnv = require("dotenv");
+const cors = require("cors");
 dotEnv.config()
 const {connectDatabase} = require("./src/modules/database/database-connection");
 const luckydrawController = require("./src/modules/lucky-draw/lucky-draw.controller")
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cors());
 app.use("/api/lucky-draw", luckydrawController)
 app.listen(PORT,() => {
     console.log(`Server listening on ${PORT}`)
