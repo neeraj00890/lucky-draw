be060e8f13d8dc157fe41e469b78a57942f2836a
neeraj00890 <kumar74312@gmail.com>
2022-04-08 14:42:01 +0530

    fix: name

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const dotEnv = require("dotenv");
 dotEnv.config()
 const {connectDatabase} = require("./src/modules/database/database-connection");
-const luckydrawControler = require("./src/modules/lucky-draw/lucky-draw.controller")
+const luckydrawController = require("./src/modules/lucky-draw/lucky-draw.controller")
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
-app.use("/api/lucky-draw", luckydrawControler)
+app.use("/api/lucky-draw", luckydrawController)
 app.listen(PORT,() => {
     console.log(`Server listening on ${PORT}`)
     connectDatabase()
