46b9cc7cdd005040d0b7e8c9969f4a326abdf7f7
Sandeep Yadav <svankala@webmobinfo.ch>
2022-04-20 23:47:10 +0530

    Added new method

--- src/modules/lucky-draw/lucky-draw.service.js
+++ src/modules/lucky-draw/lucky-draw.service.js
@@ -1,6 +1,12 @@
-const { getRandomNumber, getRandomNumberFromArray } = require("../common/common.utils");
+const {
+  getRandomNumber,
+  getRandomNumberFromArray,
+} = require("../common/common.utils");
 
-const { NUMBER_THOUSAND, WINNING_TYPES } = require("../common/common-constants");
+const {
+  NUMBER_THOUSAND,
+  WINNING_TYPES,
+} = require("../common/common-constants");
 const messages = require("../common/response-messages");
 
 const WinningPrice = require("./winning-prize.model");
@@ -22,58 +28,69 @@ function getSilverList(blackListNumbers) {
 }
 
 function getBrownList(blackListNumbers) {
-  const oneToThousand = Array.from({ length: NUMBER_THOUSAND }, (_, i) => i + 1);
-  return oneToThousand.filter(number => !blackListNumbers.includes(number));
+  const oneToThousand = Array.from(
+    { length: NUMBER_THOUSAND },
+    (_, i) => i + 1
+  );
+  return oneToThousand.filter((number) => !blackListNumbers.includes(number));
 }
 
 async function handleGoldWinner(address, winningPrizes, randomNumber) {
-  const returnObject = {}
+  const returnObject = {};
   returnObject.winningType = WINNING_TYPES.GOLD;
-      await LuckydrawRedemption.create({
-        address,
-        winningType: WINNING_TYPES.GOLD,
-        winningNumber: randomNumber
-      });
-      winningPrizes.availableGoldPrizes = winningPrizes.availableGoldPrizes.filter(goldPrizeNumber => goldPrizeNumber != randomNumber);
-      winningPrizes.save();
-      return returnObject;
+  await LuckydrawRedemption.create({
+    address,
+    winningType: WINNING_TYPES.GOLD,
+    winningNumber: randomNumber,
+  });
+  winningPrizes.availableGoldPrizes = winningPrizes.availableGoldPrizes.filter(
+    (goldPrizeNumber) => goldPrizeNumber != randomNumber
+  );
+  winningPrizes.save();
+  return returnObject;
 }
 
 async function handleSilverWinner(address, winningPrizes, randomNumber) {
-  const returnObject = {}
+  const returnObject = {};
   returnObject.winningType = WINNING_TYPES.SILVER;
-      await LuckydrawRedemption.create({
-        address,
-        winningType: WINNING_TYPES.SILVER,
-        winningNumber: randomNumber
-      });
-      winningPrizes.availableSilverPrizes = winningPrizes.availableSilverPrizes.filter(silverPrizeNumber => silverPrizeNumber != randomNumber);
-      winningPrizes.save();
-      return returnObject;
+  await LuckydrawRedemption.create({
+    address,
+    winningType: WINNING_TYPES.SILVER,
+    winningNumber: randomNumber,
+  });
+  winningPrizes.availableSilverPrizes =
+    winningPrizes.availableSilverPrizes.filter(
+      (silverPrizeNumber) => silverPrizeNumber != randomNumber
+    );
+  winningPrizes.save();
+  return returnObject;
 }
 
 async function handleBrownWinner(address, winningPrizes, randomNumber) {
-  const returnObject = {}
+  const returnObject = {};
   returnObject.winningType = WINNING_TYPES.BROWN;
-      await LuckydrawRedemption.create({
-        address,
-        winningType: WINNING_TYPES.BROWN,
-        winningNumber: randomNumber
-      });
-      winningPrizes.availablebrownPrizes = winningPrizes.availablebrownPrizes.filter(brownPrizeNumber => brownPrizeNumber != randomNumber);
-      winningPrizes.save();
-      return returnObject;
+  await LuckydrawRedemption.create({
+    address,
+    winningType: WINNING_TYPES.BROWN,
+    winningNumber: randomNumber,
+  });
+  winningPrizes.availablebrownPrizes =
+    winningPrizes.availablebrownPrizes.filter(
+      (brownPrizeNumber) => brownPrizeNumber != randomNumber
+    );
+  winningPrizes.save();
+  return returnObject;
 }
 
 exports.initializeWinningPrize = async function () {
-  const  initialCount = await WinningPrice.countDocuments({});
-  console.log(initialCount)
-  if(initialCount > 0) {
+  const initialCount = await WinningPrice.countDocuments({});
+  console.log(initialCount);
+  if (initialCount > 0) {
     return {
-      message: messages.DATA_EXISTS
-    }
+      message: messages.DATA_EXISTS,
+    };
   }
-   
+
   const goldPrizes = getGoldList();
   const silverPrizes = getSilverList(goldPrizes);
   const brownPrizes = getBrownList([...silverPrizes, ...goldPrizes]);
@@ -83,41 +100,88 @@ exports.initializeWinningPrize = async function () {
     brownPrizes,
     availableGoldPrizes: goldPrizes,
     availableSilverPrizes: silverPrizes,
-    availablebrownPrizes: brownPrizes
+    availablebrownPrizes: brownPrizes,
   });
 
   return {
-    message: messages.SUCCESS_INIT_SCRIPT
-  }
+    message: messages.SUCCESS_INIT_SCRIPT,
+  };
 };
+
+async function checkAlready(address) {
+  const res = await LuckydrawRedemption.findOne({ address: address });
+  if (res !== null) {
+    console.log("Already Present");
+    return res.winningType;
+  } else {
+    return null;
+  }
+}
 
 exports.redeemPrize = async function (address) {
   let returnObject = {};
-  const winningPrizes = await WinningPrice.findOne({});
-  const totalAvailableNumbers = [...winningPrizes.availableGoldPrizes, ...winningPrizes.availableSilverPrizes, ...winningPrizes.availablebrownPrizes];
-  if(totalAvailableNumbers.length) {
-    const randomNumber = getRandomNumberFromArray(totalAvailableNumbers);
-    const isGoldWinner = winningPrizes.availableGoldPrizes.includes(randomNumber);
-    const isSilverWinner = winningPrizes.availableSilverPrizes.includes(randomNumber);
-    const isBrownWinner = winningPrizes.availablebrownPrizes.includes(randomNumber);
-    console.log(address, isBrownWinner, isSilverWinner, isGoldWinner, randomNumber)
-    if (isGoldWinner) {
-      returnObject = await handleGoldWinner(address, winningPrizes, randomNumber);
-    } else if (isSilverWinner) {
-      returnObject = await handleSilverWinner(address, winningPrizes, randomNumber);
-    }
-    else if (isBrownWinner) {
-      returnObject = await handleBrownWinner(address, winningPrizes, randomNumber)
-    } 
-  } else {
-    returnObject.winningType = 'NONE(No Number Available)';
+
+  const res = await checkAlready(address);
+  if (res !== null) {
+    console.log("Returning present value");
+    returnObject.winningType = res.winningType;
+    return returnObject;
   }
- return returnObject;
+  const winningPrizes = await WinningPrice.findOne({});
+  const totalAvailableNumbers = [
+    ...winningPrizes.availableGoldPrizes,
+    ...winningPrizes.availableSilverPrizes,
+    ...winningPrizes.availablebrownPrizes,
+  ];
+  if (totalAvailableNumbers.length) {
+    const randomNumber = getRandomNumberFromArray(totalAvailableNumbers);
+    const isGoldWinner =
+      winningPrizes.availableGoldPrizes.includes(randomNumber);
+    const isSilverWinner =
+      winningPrizes.availableSilverPrizes.includes(randomNumber);
+    const isBrownWinner =
+      winningPrizes.availablebrownPrizes.includes(randomNumber);
+    console.log(
+      address,
+      isBrownWinner,
+      isSilverWinner,
+      isGoldWinner,
+      randomNumber
+    );
+    if (isGoldWinner) {
+      returnObject = await handleGoldWinner(
+        address,
+        winningPrizes,
+        randomNumber
+      );
+    } else if (isSilverWinner) {
+      returnObject = await handleSilverWinner(
+        address,
+        winningPrizes,
+        randomNumber
+      );
+    } else if (isBrownWinner) {
+      returnObject = await handleBrownWinner(
+        address,
+        winningPrizes,
+        randomNumber
+      );
+    }
+  } else {
+    returnObject.winningType = "NONE(No Number Available)";
+  }
+  return returnObject;
 };
 
-exports.fetchGoldSilverUsers = async function() {
-const data =  await LuckydrawRedemption.find({
-    $or: [{winningType: WINNING_TYPES.SILVER}, {winningType: WINNING_TYPES.GOLD}]
-  }, {address: 1, winningType: 1})
+exports.fetchGoldSilverUsers = async function () {
+  const data = await LuckydrawRedemption.find(
+    {
+      $or: [
+        { winningType: WINNING_TYPES.SILVER },
+        { winningType: WINNING_TYPES.GOLD },
+      ],
+    },
+    { address: 1, winningType: 1 }
+  );
   return data;
-}
+};
